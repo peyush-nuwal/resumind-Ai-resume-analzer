0164e35f0d2cfe689c7263ad4e86381391015796
Peyush Nuwal <piyushnawal19@gmail.com>
2025-07-21 23:00:39 +0530

    added navebar, home page and assets

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,38 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "constant";
+import ResumeCard from "~/components/ResumeCard";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your resume!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      {/* ---home page section --- */}
+      <section className="main-section py-16">
+        {/* ---home page heading --- */}
+        <div className="page-heading">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>
+            Review your submissions and get Ai-powered feedback on your resume
+          </h2>
+        </div>
+        {/* --- render some resumes --- */}
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
