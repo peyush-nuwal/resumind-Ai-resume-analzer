e47538f12fc022f8a19787fe918c1238b61431e3
Peyush Nuwal <piyushnawal19@gmail.com>
2025-07-25 23:29:05 +0530

    fetch real resume on homepage

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,36 +1,63 @@
-import React from 'react'
-import { Link } from 'react-router'
-import ScoreCircle from './ScoreCircle';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router";
+import ScoreCircle from "./ScoreCircle";
+import { usePuterStore } from "~/lib/puter";
 
-const ResumeCard = ({ resume }: { resume: Resume }) => {
+const ResumeCard = ({
+  companyName,
+  jobTitle,
+  imagePath,
+  id,
+  feedback,
+}: Resume & { feedback: Feedback }) => {
+  const { fs } = usePuterStore();
+  const [resumeUrl, setResumeUrl] = useState<string>();
+
+  useEffect(() => {
+    const fetchResumes = async () => {
+      const blob = await fs.read(imagePath);
+      if (!blob) return;
+      let url = URL.createObjectURL(blob);
+      setResumeUrl(url);
+    };
+    fetchResumes();
+  }, [imagePath]);
   return (
     <Link
-      to={`/resume/${resume.id}`}
+      to={`/resume/${id}`}
       className="resume-card animate-in fade-in  duration-1000"
-      >
-        {/* ---resume-card-header --- */}
+    >
+      {/* ---resume-card-header --- */}
       <div className="resume-card-header">
         <div className="flex flex-col gap-2">
-          <h2 className="!text-black font-bold break-words">
-            {resume.companyName}
-          </h2>
-          <h3 className="text-lg break-words text-gray-500 ">
-            {resume.jobTitle}
-          </h3>
+          {companyName && (
+            <h2 className="!text-black font-bold break-words">{companyName}</h2>
+          )}
+          {jobTitle && (
+            <h3 className="text-lg break-words text-gray-500 ">{jobTitle}</h3>
+          )}
+          {!companyName && !jobTitle && (
+            <h3 className=" font-bold !text-black ">Resume</h3>
+          )}
         </div>
         <div className="flex-shrink-0">
-          <ScoreCircle score={resume.feedback.overallScore} />
+          <ScoreCircle score={feedback.overallScore} />
         </div>
       </div>
-          {/* ---resume-card-image --- */}
-          <div className="gradient-border animate-in fade-in duration-1000">
-              <div className="w-full h-full">
-                  
-      <img src={resume.imagePath} alt={resume.jobTitle} className='w-full h-[350px] max-sm:h-[20px] object-cover' />
-              </div>
+      {/* ---resume-card-image --- */}
+      {resumeUrl && (
+        <div className="gradient-border animate-in fade-in duration-1000">
+          <div className="w-full h-full">
+            <img
+              src={resumeUrl}
+              alt={jobTitle}
+              className="w-full h-[350px] max-sm:h-[20px] object-cover"
+            />
           </div>
+        </div>
+      )}
     </Link>
   );
-}
+};
 
-export default ResumeCard
+export default ResumeCard;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,9 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { resumes } from "constant";
 import ResumeCard from "~/components/ResumeCard";
 import { usePuterStore } from "~/lib/puter";
-import { useNavigate } from "react-router";
-import { useEffect } from "react";
-
+import { Link, useNavigate } from "react-router";
+import { useEffect, useState } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -15,15 +13,29 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-     const { isLoading, auth } = usePuterStore();
-  
-     const navigate = useNavigate();
+  const { auth, kv } = usePuterStore();
+  const navigate = useNavigate();
+  const [resumes, setResumes] = useState<Resume[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-     useEffect(() => {
-       if (!auth.isAuthenticated) {
-         navigate("/auth?next=/")
-       }
-     }, [auth.isAuthenticated]);
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      navigate("/auth?next=/");
+    }
+  }, [auth.isAuthenticated]);
+
+  useEffect(() => {
+    const fetchResumes = async () => {
+      setIsLoading(true);
+      const resumes = (await kv.list("resume:*", true)) as unknown as KVItem[];
+      if (!resumes) return;
+      const parsedResumes = resumes.map((resume) => JSON.parse(resume.value));
+      setResumes(parsedResumes);
+      setIsLoading(false);
+    };
+    fetchResumes();
+  }, []);
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
@@ -32,16 +44,28 @@ export default function Home() {
         {/* ---home page heading --- */}
         <div className="page-heading">
           <h1>Track Your Applications & Resume Ratings</h1>
-          <h2>
-            Review your submissions and get Ai-powered feedback on your resume
-          </h2>
+          {!isLoading && resumes.length === 0 ? (
+            <h2>No resumes found. upload your resume to get started</h2>
+          ) : (
+            <h2>
+              Review your submissions and get Ai-powered feedback on your resume
+            </h2>
+          )}
         </div>
+        {isLoading && <div className="flex justify-center items-center h-full">
+          <img src="/images/resume-scan-2.gif" alt="scaning resumes" className="w-[200px]" />
+        </div>}
         {/* --- render some resumes --- */}
-        {resumes.length > 0 && (
+        {!isLoading && resumes.length > 0 && (
           <div className="resumes-section">
             {resumes.map((resume) => (
-              <ResumeCard key={resume.id} resume={resume} />
+              <ResumeCard key={resume.id} {...resume} />
             ))}
+          </div>
+        )}
+        {!isLoading && resumes.length === 0 && (
+          <div className="flex justify-center items-center h-full mt-10 gap-4">
+             <Link to="/upload" className="primary-button w-fit text-xl font-semibold">Upload Resume</Link>
           </div>
         )}
       </section>
